4e05fb38d926b8783a985a2b37e4ad5b0db9b580
Animesh Reddy <66461550+gsaianimesh@users.noreply.github.com>
2024-11-13 09:16:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -134,8 +134,8 @@ const updateTasksList = () => {
                 <small>${timeLeftText}</small>
             </div>
             <div class="icons">
-                <img src="./img/edit.png" onclick="editTask(${index})" />
-                <img src="./img/bin.png" onclick="deleteTask(${index})" />
+                <img src="./edit.png" onclick="editTask(${index})" />
+                <img src="./bin.png" onclick="deleteTask(${index})" />
             </div>
         `;
 
